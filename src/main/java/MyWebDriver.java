92f68e9e1149ba88ec3d26d3f4508878f0fcdd0b
dailai <837833280@qq.com>
2018-11-11 22:33:49 +0800

    提交项目

---
+++ src/main/java/MyWebDriver.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by deng on 2017/5/16.
+ */
+public class MyWebDriver {
+//    private final static String CHROME_DRIVER_PATH = "/Users/deng/IdeaProjects/Spider/src/chromedriver";
+    private final static String CHROME_DRIVER_PATH = "driver/chromedriver.exe";
+
+    public static WebDriver createWebDriver() {
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
+        return new ChromeDriver();
+    }
+}
